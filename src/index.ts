ce03c92323e5372a9129d5e5ee287b2e93ec684c
hanzhangsan <a19965267385@163.com>
2025-12-10 09:12:29 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono'
 import { hello } from './api/hello'
+import { login } from './api/login'
 
 const app = new Hono()
 
 app.get('/api/hello', hello)
+app.post('/api/login', login)
 
 export default app
