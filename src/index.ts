494e0a5c619351f4de86f153380000faf35f4edb
hanzhangsan <a19965267385@163.com>
2025-12-10 08:25:03 +0800

    路由文件

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Hono } from 'hono'
+import hello from './api/hello'
+// import user from './api/user'
+// import login from './api/login'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-app.get('/home', (c) => {
-  return c.text('Hello home!')
-})
+app.route('/api/hello', hello)
+// app.route('/api/user', user)
+// app.route('/api/login', login)
 
 export default app
