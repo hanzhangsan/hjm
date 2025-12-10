fac3e29a0efad0ae56f11830785f5b189879bbaf
hanzhangsan <a19965267385@163.com>
2025-12-10 11:31:49 +0800

    Update login.ts

--- src/api/login.ts
+++ src/api/login.ts
@@ -3,17 +3,25 @@ import { Hono , Context} from 'hono'
 
 export const login = async (c: Context) => {
     const json = await c.req.json<{name:string;password:string}>()
+    const db = c.env.MYSQL
     if(!json.name||!json.password){
          return c.json({
             code:400,
             msg:"账户名称与密码不能为空"
          },400)
     }
+    var user = await db
+    .prepare('SELECT * FROM users WHERE name = ?')
+    .bind(json.name)
+    .get()
+    if(!user){
+        return c.json({
+            code:400,
+            msg:"用户不存在"
+        })
+    }
     return c.json({
         code:200,
-        data:{
-            name:json.name,
-            password:json.name
-        }
+        data:user
     },200)
 }
