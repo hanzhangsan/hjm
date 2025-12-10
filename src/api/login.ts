9658193f9f410cc6b170c479a9662955703682d0
hanzhangsan <a19965267385@163.com>
2025-12-10 09:21:20 +0800

    Update login.ts

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,6 +2,18 @@ import { serve } from '@hono/node-server'
 import { Hono , Context} from 'hono'
 
 export const login = async (c: Context) => {
-    const json = await c.req.json<{name:string}>()
-    return c.text(json.name)
+    const json = await c.req.json<{name:string;password:string}>()
+    if(!json.name||!json.password){
+         return c.json({
+            code:400,
+            msg:"账户名称与密码不能为空"
+         },400)
+    }
+    return c.json({
+        code:200,
+        data:{
+            name:json.name,
+            password:json.name
+        }
+    },200)
 }
